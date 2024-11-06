5b8042163ad35b4d3c48482c8f724c03e5cf2850
Vunky Himawan <tingkerbeii44@gmail.com>
2024-11-06 23:15:42 +0700

    delete some unused files and directories

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from .interface.http.api.routes.test_routes import router as test
-from .interface.http.api.routes.user_routes import router as user_routes
 from .interface.http.api.routes.authentication.authentication_routes import router as authentication_routes
 from dotenv import load_dotenv
 
@@ -9,6 +7,4 @@ load_dotenv()
 app = FastAPI(title="Eventpass API", version="1.0.0")
 
 # Include routers
-# app.include_router(test, prefix="/api/v1", tags=["Test"])
 app.include_router(authentication_routes, prefix="/api/v1/auth", tags=["Authentication"])
-# app.include_router(user_routes, prefix="/api/v1", tags=["Users"])
